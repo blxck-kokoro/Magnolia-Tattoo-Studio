baa58ab68fe858807a60babbb7c30f4d87b93ee4
jacob <blxckkokoro@gmail.com>
2023-03-16 10:53:53 +0100

    Added mobile animation onclick for artists' portraits

--- Landing Page/artistInfoDisplay.js
+++ Landing Page/artistInfoDisplay.js
@@ -1,26 +1,29 @@
 const artistPortrait = document.querySelectorAll('.artist-portrait');
-const artistPortraitContent = document.querySelectorAll('.artist-portrait-content');
 
-for (let i = 0; i < artistPortrait .length; i++) {
-    artistPortrait [i].addEventListener('mouseover', () => {
-        if (artistPortrait[i].parentNode.classList.contains('artist-odd')) {
-            artistPortrait[i].style.transform = 'translateX(1rem)'  
-        } else if (artistPortrait[i].parentNode.classList.contains('artist-even')) {
-            artistPortrait[i].style.transform = 'translateX(-1rem)'
-        }
-        artistPortrait[i].style.transition = '0.5s'
-        artistPortraitContent[i].style.opacity = '0.7';
-        artistPortraitContent[i].style.transition = '0.5s';
-    }, false)
+function displayArtistInfo() {
+    if (this.parentNode.classList.contains('artist-odd')) {
+        this.style.transform = 'translateX(1rem)'  
+    } else if (this.parentNode.classList.contains('artist-even')) {
+        this.style.transform = 'translateX(-1rem)'
+    }
+    this.style.transition = '0.5s'
+    this.firstChild.style.opacity = '0.7';
+    this.firstChild.style.transition = '0.5s';
+}
 
-    artistPortrait [i].addEventListener('mouseout', () => {
-        if (artistPortrait[i].parentNode.classList.contains('artist-odd')) {
-            artistPortrait[i].style.transform = 'translateX(-1rem)'  
-        } else if (artistPortrait[i].parentNode.classList.contains('artist-even')) {
-            artistPortrait[i].style.transform = 'translateX(1rem)'
-        }
-        artistPortrait[i].style.transition = '0.5s'
-        artistPortraitContent[i].style.opacity = '0';
-        artistPortraitContent[i].style.transition = '0.5s';
-    }, false)
+function hideArtistInfo() {
+    if (this.parentNode.classList.contains('artist-odd')) {
+        this.style.transform = 'translateX(-1rem)'  
+    } else if (this.parentNode.classList.contains('artist-even')) {
+        this.style.transform = 'translateX(1rem)'
+    }
+    this.style.transition = '0.5s'
+    this.firstChild.style.opacity = '0';
+    this.firstChild.style.transition = '0.5s';
+}
+
+for (let i = 0; i < artistPortrait.length; i++) {
+    artistPortrait[i].addEventListener('mouseover', displayArtistInfo, false);
+    artistPortrait[i].addEventListener('mouseout', hideArtistInfo, false)
+    artistPortrait[i].addEventListener('click', displayArtistInfo, false);
 };
